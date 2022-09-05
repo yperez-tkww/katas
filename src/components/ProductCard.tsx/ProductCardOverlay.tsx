0b47b9daf86f02e90800d2cc848ea98da42f27f5
Yanira Perez <yperez@bodas.net>
2022-09-05 18:03:10 +0200

    create ProductCardOverlay

---
+++ src/components/ProductCard.tsx/ProductCardOverlay.tsx
@@ -0,0 +1,25 @@
+import { FC } from 'react';
+import { Product } from '../../types';
+
+const ProductCardOverlay: FC<Product> = ({
+  name,
+  image,
+  description,
+  price,
+}) => {
+  return (
+    <div
+      className="cardOverlayContainer"
+      // style={{ background: `url(${image})`, backgroundSize: 'cover' }}
+    >
+      <div className="overlayContent">
+        <strong className="productOverlayName">{name}</strong>
+        <span className="productOverlayDescription">{description}</span>
+        <span className="productOverlayPrice">{price} €</span>
+        <button>Add to cart 🤑</button>
+      </div>
+    </div>
+  );
+};
+
+export default ProductCardOverlay;
